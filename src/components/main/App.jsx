ce1f6abe3b3fe4eb80aa6c0737fb1bdc6cd4f27c
Thiago Morais <thiagomorais@projetosthiago.com.br>
2019-05-12 22:52:57 -0300

    Criada as estruturas dos componentes

---
+++ src/components/main/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+import Logo from '../template/Logo';
+import Nav from '../template/Nav';
+import Main from '../template/Main';
+import Footer from '../template/Footer';
+
+export default props => (
+  <div className="app">
+    <Logo />
+    <Nav />
+    <Main />
+    <Footer />
+  </div>
+);
