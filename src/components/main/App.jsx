b140b0e6012fbd6b1ae8ab9015d8a5c452c6e82f
Thiago Morais <thiagomorais@projetosthiago.com.br>
2019-05-13 00:40:14 -0300

    Commit configurando o arrow-keys-react

--- src/components/main/App.jsx
+++ src/components/main/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ArrowKeysReact from 'arrow-keys-react';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,10 +9,53 @@ import Nav from '../template/Nav';
 import Main from '../template/Main';
 import Footer from '../template/Footer';
 
-export default props => (
-  <div className="app">
-    <Nav />
-    <Main />
-    <Footer />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: 'Use arrow key',
+      nav: false,
+      main: true,
+      footer: false,
+      index: 0,
+    };
+
+    ArrowKeysReact.config({
+      left: () => {
+        this.setState({
+          content: 'left key detected.',
+        });
+      },
+      right: () => {
+        this.setState({
+          content: 'right key detected.',
+        });
+      },
+      up: () => {
+        this.setState({
+          content: 'up key detected.',
+        });
+      },
+      down: () => {
+        this.setState({
+          content: 'down key detected.',
+        });
+      },
+    });
+  }
+
+  render() {
+    const { state } = this;
+
+    return (
+      <div className="app" {...ArrowKeysReact.events} tabIndex="1">
+        <div>{state.content}</div>
+        <Nav selected={state.nav} index={state.index} />
+        <Main selected={state.main} index={state.index} />
+        <Footer selected={state.footer} index={state.index} />
+      </div>
+    );
+  }
+}
+
+export default App;
